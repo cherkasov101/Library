e0c303888a0deaad08f8fc1e770bea5e437c63f2
Юрий Черкасов <cherkasov1010@gmail.com>
2023-01-21 16:30:45 +0300

    added classes User and Admin

--- Book.cs
+++ Book.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Library
+﻿ namespace Library
 {
     public class Book
     {
-        private string name; // название книги 
+        private static int nextId = 1; // id для следующей созданной книги
+        private int id; // id книги
+        private string title; // название книги 
         private string author; // автор книги
 
         // констуктор класс Book, создаёт объект данного класса
-        public Book(string name, string author)
+        public Book(string title, string author)
         {
-            this.name = name;
+            id = nextId;
+            nextId++;
+            this.title = title;
             this.author = author;
         }
 
+        // свойство поля id, позволяет получать id книги
+        public int Id { 
+            get { return id; } 
+        }
+
         // свойства поля name, позволяют получать название книги и устанавливать его
-        public string Name
+        public string Title
         {
-            get { return name; }
-            set { name = value; }
+            get { return title; }
+            set { title = value; }
         }
 
         // свойства поля author, позволяют получать имя автора  книги и устанавливать его
---
+++ Catalog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Catalog
+    {
+        private Dictionary<Book, int> catalog; // каталог книг
+
+        // конструктор, создаёт объект класса Catalog
+        public Catalog()
+        {
+
+        }
+    }
+}
