76b1b82f86e2f0f71bb286fbb416937b1b86e1e5
Юрий Черкасов <cherkasov1010@gmail.com>
2023-01-22 15:02:12 +0300

    added methods to Admin, User and Book

--- Admin.cs
+++ Admin.cs
@@ -6,10 +6,83 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class Admin
+    public class Admin
     {
         private string name; //
-        private string id; //
+        private int id; //
         private string password; //
+        private static int nexId = 100; //
+
+        //
+        public Admin(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+            this.id = nexId;
+            nexId++;
+        }
+
+        //
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        //
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        //
+        public void AddBook()
+        {
+            
+        }
+
+        //
+        public void RemoveBook()
+        {
+
+        }
+
+        //
+        public void EditBookTitle(Book book, string newTitle)
+        {
+            book.Title = newTitle;
+        }
+
+        //
+        public void EditBookAuthor(Book book, string newAuthor)
+        {
+            book.Author = newAuthor;
+        }
+
+        //
+        public void EditDescription(Book book, string newDescription)
+        {
+            book.Description = newDescription;
+        }
+
+        //
+        public void GiveBook()
+        {
+
+        }
+
+        //
+        public void GetBook()
+        {
+
+        }
     }
 }
--- User.cs
+++ User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class User
+    public class User
     {
         private string name; //
         private DateTime birthday; //
@@ -15,5 +15,81 @@ namespace Library
         private static int nextTicket = 1000000; //
         private List<Book> onHandBooks; //
         private List<Book> returnedBooks; //
+
+        //
+        public User(string name, string day, string month, string year, string phoneNumber)
+        {
+            this.name = name;
+            birthday = new DateTime();
+            this.phoneNumber = phoneNumber;
+            ticketNumber = nextTicket;
+            nextTicket++;
+            onHandBooks = new List<Book>();
+            returnedBooks = new List<Book>();
+        } 
+
+        //
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set { birthday = value; }
+        }
+
+        //
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        //
+        public int TicketNumber
+        {
+            get { return ticketNumber; }
+        }
+
+        //
+        public List<Book> OnHandBooks
+        {
+            get { return onHandBooks; }
+        }
+
+        //
+        public List<Book> ReturnedBooks
+        {
+            get { return returnedBooks; }
+        }
+
+
+        //
+        public Catalog ViewAllBooks() 
+        {
+            return new Catalog();
+        }
+
+        //
+        public Book SearchBookByAuthor()
+        {
+            return new Book("", "");
+        }
+
+        //
+        public Book SearchBookByTitle()
+        {
+            return new Book("", "");
+        }
+
+        //
+        public void SubmitApplication()
+        {
+
+        }
     }
 }
